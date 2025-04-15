f6e8cb6c3cceb5ecb90337983349716345e421d2
Krunal Darekar <kruanl.darekar@aisensy.com>
2025-04-15 18:30:15 +0530

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by krunal darekar on 15/04/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
